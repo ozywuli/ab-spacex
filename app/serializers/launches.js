8e5e29f66e1ff47a9c04db45777d4aa25c86a703
Ozy Wu-Li <ozy.wu.lI@gmail.com>
2019-03-18 15:16:52 -0700

    refactor duplicate code in launches and rockets serializers

--- app/serializers/launches.js
+++ app/serializers/launches.js
@@ -1,34 +1,27 @@
 import DS from 'ember-data';
 
+function mapLaunches(attrs, type) {
+  return {
+    id: `${attrs.flight_number}`,
+    type: type.modelName,
+    attributes: {
+      title: attrs.mission_name,
+      imgUrl: attrs.links.mission_patch,
+      articleLink: attrs.links.article_link,
+      description: attrs.details,
+    }
+  }
+}
+
 export default DS.JSONAPISerializer.extend({
   normalizeFindAllResponse(store, type, payload) {
     return {
-      data: payload.map(attrs => (
-        {
-          id: attrs.flight_number,
-          type: type.modelName,
-          attributes: {
-            title: attrs.mission_name,
-            imgUrl: attrs.links.mission_patch,
-            articleLink: attrs.links.article_link,
-            description: attrs.details,
-          }
-        }
-      ))
+      data: payload.map(attrs => mapLaunches(attrs, type))
     }
   },
   normalizeFindRecordResponse(store, type, payload) {
     return {
-      data: {
-        id: `${payload.flight_number}`,
-        type: type.modelName,
-        attributes: {
-          title: payload.mission_name,
-          imgUrl: payload.links.mission_patch,
-          articleLink: payload.links.article_link,
-          description: payload.details,
-        }
-      }
+      data: mapLaunches(payload, type)
     }
   },
 });
--- app/serializers/rockets.js
+++ app/serializers/rockets.js
@@ -1,35 +1,28 @@
 import DS from 'ember-data';
 
+function mapRockets(attrs, type) {
+  return {
+    id: `${attrs.rocket_id}`,
+    type: type.modelName,
+    attributes: {
+      noId: attrs.id,
+      title: attrs.rocket_name,
+      imgUrl: attrs.flickr_images[0],
+      articleLink: attrs.wikipedia,
+      description: attrs.description,
+    }
+  }
+}
+
 export default DS.JSONAPISerializer.extend({
   normalizeFindAllResponse(store, type, payload) {
     return {
-      data: payload.map(attrs => (
-        {
-          type: 'rockets',
-          id: attrs.rocket_id,
-          attributes: {
-            title: attrs.rocket_name,
-            imgUrl: attrs.flickr_images[0],
-            articleLink: attrs.wikipedia,
-            description: attrs.description,
-          }
-        }
-      ))
+      data: payload.map(attrs => mapRockets(attrs, type))
     }
   },
   normalizeFindRecordResponse(store, type, payload) {
     return {
-      data: {
-        id: `${payload.rocket_id}`,
-        type: type.modelName,
-        attributes: {
-          noId: payload.id,
-          title: payload.rocket_name,
-          imgUrl: payload.flickr_images[0],
-          articleLink: payload.wikipedia,
-          description: payload.description,
-        }
-      }
+      data: mapRockets(payload, type)
     }
   },
 });
