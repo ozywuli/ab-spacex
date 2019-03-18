c1c899c47ec6c6f1a2aa99ab3cce0a8a91fc7d94
Ozy Wu-Li <ozy.wu.lI@gmail.com>
2019-03-18 15:37:33 -0700

    make launches controller share code with rockets controller - index route should also redirect to launches route

--- app/controllers/rockets.js
+++ app/controllers/rockets.js
@@ -1,12 +1,5 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['layout'],
-  layout: 'grid',
 
-  actions: {
-    toggleLayout(style) {
-      this.set('layout', style);
-    }
-  }
 });
--- app/routes/rockets.js
+++ app/routes/rockets.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  controllerName: 'launches',
   model() {
     return this.store.findAll('rockets');
   }
